55548fb12f70198dc5e07b209516b4719d5d45d3
Hardik Sarkar <107950304+hardiksarkar@users.noreply.github.com>
2023-08-25 09:45:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function volume_sphere() {
     //Write your code here
-  
-} 
+	let radius = document.getElementById("radius");
+      let volume = document.getElementById("volume")
+      volume.value = (4/3)*(Math.PI)*Math.pow(Number(radius.value),2);   
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+} 
+window.onload = document.getElementById('MyForm').addEventListener('submit',(event)=>{
+     event.preventDefault();
+     volume_sphere();
+ })
+
+// window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
